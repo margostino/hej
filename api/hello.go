2de4cba962f348bf9f41dcfd6f46e0e2606eee84
Martin D'Agostino <martin.dagostino@klarna.com>
2022-03-28 15:31:07 +0200

    env variable

--- api/hello.go
+++ api/hello.go
@@ -3,8 +3,9 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+	fmt.Fprintf(w, "Hello World!", os.Getenv("TEST_SUPER_KEY"))
 }

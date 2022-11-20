3fdaf7d17dcaf840a5f365f8ff2774703de222d3
Martin D'Agostino <martin.dagostino@klarna.com>
2022-11-20 20:18:16 +0100

    test web scrapper

--- api/hello.go
+++ api/hello.go
@@ -2,9 +2,25 @@ package handler
 
 import (
 	"fmt"
+	"github.com/go-rod/rod"
 	"net/http"
 )
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
+	fmt.Fprintf(w, "Hello World!\n")
+
+	url := "https://en.wikipedia.org/wiki/The_Lord_of_the_Rings"
+	browser := rod.New().MustConnect()
+	defer browser.Close()
+
+	page := browser.MustPage(url).MustWaitLoad()
+	bodyElements, err := page.Elements(".infobox.vcard")
+	if err != nil {
+		fmt.Printf("Error: %s\n", err.Error())
+	}
+
+	for _, element := range bodyElements {
+		text, _ := element.Text()
+		fmt.Println(text)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module hej
 go 1.17
 
 require (
+	github.com/go-rod/rod v0.112.0 // indirect
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	github.com/ysmood/goob v0.4.0 // indirect
+	github.com/ysmood/gson v0.7.1 // indirect
+	github.com/ysmood/leakless v0.8.0 // indirect
 )
